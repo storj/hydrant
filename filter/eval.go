39888b0bf62e7f5fb1c16f50ddc1b02803a46448
Jeff Wendling <leterip@gmail.com>
2025-11-19 18:01:34 -0500

    oops missed gofmt

--- filter/eval.go
+++ filter/eval.go
@@ -133,27 +133,3 @@ func (es *EvalState) Lookup(key string) (value.Value, bool) {
 
 	return value.Value{}, false
 }
-
-func pop[T any](es *EvalState, convert func(value.Value) (T, bool)) (t T, ok bool) {
-	if n := len(es.stack); n > 0 {
-		v := es.stack[n-1]
-		es.stack = es.stack[:n-1]
-		t, ok = convert(v)
-	}
-	return t, ok
-}
-
-func peek[T any](es *EvalState, convert func(value.Value) (T, bool)) (t T, ok bool) {
-	if n := len(es.stack); n > 0 {
-		t, ok = convert(es.stack[n-1])
-	}
-	return t, ok
-}
-
-func anyfy(vs []value.Value) []any {
-	av := make([]any, len(vs))
-	for i, v := range vs {
-		av[i] = v.AsAny()
-	}
-	return av
-}
